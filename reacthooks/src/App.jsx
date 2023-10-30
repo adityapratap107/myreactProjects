79f16f590cc20ff959cf10e21b58c3a3ba0df415
Aditya Pratap Singh <aditya.singh@netsolutions.com>
2023-10-31 01:18:50 +0530

    first commit

---
+++ reacthooks/src/App.jsx
@@ -0,0 +1,47 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Tailwind from './tailwind/Tailwind'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <div>
+        <h1>count {count}</h1>
+      </div>
+      <div className="card">
+        <button onClick={() => {
+          // setCount((count) => count + 1)
+          // setCount((count) => count + 1)
+          // setCount((count) => count + 1)
+          // setCount((count) => count + 1)
+
+          setCount(count + 1)
+          setCount(count + 1)
+          setCount(count + 1)
+          setCount(count + 1)
+        }}>
+         Add Value
+        </button>
+        <button onClick={() => {
+          if(count > 0) {
+            setCount((count) => count - 1)
+          }
+        }}>
+         Remove Value
+        </button>
+       
+      </div>
+     
+
+
+
+    <Tailwind/>
+    </>
+  )
+}
+
+export default App
---
+++ reacthooks/src/tailwind/Tailwind.jsx
@@ -0,0 +1,17 @@
+import '../App.css'
+import Card from './components/Card'
+
+function Tailwind() {
+
+  return (
+    <>
+    <h1 className='bg-green-400 text-black p-4 rounded-xl mb-4'>Tailwind</h1>
+    
+   <Card/>
+   <Card/>
+
+    </>
+  )
+}
+
+export default Tailwind
